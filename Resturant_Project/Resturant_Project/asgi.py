70ea554a2b518a89681df525115f8877c87c5cd5
Angelvij <angelvij8@gmail.com>
2026-01-31 16:49:04 +0530

    Update asgi.py

--- Resturant_Project/Resturant_Project/asgi.py
+++ Resturant_Project/Resturant_Project/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resturant_Project.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'restaurant_management.settings'
+)
 
 application = get_asgi_application()
+
